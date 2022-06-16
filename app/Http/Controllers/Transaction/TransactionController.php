526b7fcbce4afdbfdbe9102863cdb8ec3763625d
XAGAN <amirxagan@gmail.com>
2022-06-16 14:43:14 +0430

    balance

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Transaction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
@@ -17,10 +18,15 @@ class TransactionController extends Controller
             'network' => $request->network,
         ]);
 
+        $user_id = Auth::id();
+        $amount = $request->amount;
+        $asset = $request->asset;
+        $old_balance = DB::select("select balance from wallet where user_id='$user_id' AND asset='$asset",);
+        $balance = $old_balance + $amount;
+        $deposit = DB::update("update wallet set balance = '$balance' where user_id='$user_id' AND asset='$asset'",);
 
         return response()
-            ->json(['data' => "Sor Sihdir Riza",]);
+            ->json(['data' => "Transaction Completed",]);
     }
-
-    
+  
 }
