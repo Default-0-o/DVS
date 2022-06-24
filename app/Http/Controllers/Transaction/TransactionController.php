218f515920ac46aed33e56439a472a8d2c7ef0e6
XAGAN <amirxagan@gmail.com>
2022-06-24 12:49:46 +0430

    transaction

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class TransactionController extends Controller
 {
