48872c8010e85b55a3fec4ef00ebcaccacf78acd
XAGAN <amirxagan@gmail.com>
2022-06-24 16:45:21 +0430

    transaction data api

--- app/Http/Controllers/API/DbController.php
+++ app/Http/Controllers/API/DbController.php
@@ -31,7 +31,7 @@ class DbController extends Controller{
     }
 
     public function transactionData(Request $request){
-        $transaction = DB::table('transactions')->get();
+        $transaction = DB::table('transactions')->select('id', 'user_id', 'network', 'transaction_id', 'address', 'direction', 'status', 'asset', 'amount', 'time_stamp', 'updated_at', 'created_at')->get();
         $query = "SELECT * FROM `transactions` WHERE 1";
         return response()
             ->json(['data' => $transaction, 'query' => $query,]);
